a837ac59f00b0a1e0966d03773bc673f52f7c30b
sekikawamiki <sekikawa@hoku.co.jp>
2026-02-17 16:46:57 +0900

    jquery3系→4系にアップデート

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ const cssSass = () => {
 //js
 const js = () => {
     return src([
-		'src/asset/js/jquery-3.7.1.min.js',
+		'src/asset/js/jquery-4.0.0.min.js',
 		'src/asset/js/slick.min.js',
 		'src/asset/js/jquery.magnific-popup.min.js',
 		'src/asset/js/run.js',
